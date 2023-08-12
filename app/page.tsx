bfa1544e7b096eb3a2db9727a12e432b620ea069
Nicky Hama <njhama@usc.edu>
2023-08-11 23:34:18 -0700

    fully functional

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 
 "use client"
 import Image from 'next/image'
-import  {useEffect} from 'react';
+import  {useEffect, useState} from 'react';
 import mapboxgl from 'mapbox-gl'
 import io from 'socket.io-client';
 import {API_KEY1} from '../config.js'
+import 'mapbox-gl/dist/mapbox-gl.css'; 
 
 export default function Home() {
+  const [markerOn, setMarkerOn] = useState(false);
+  const [marker, setMarker] = useState<mapboxgl.Marker | null>(null);
+
   useEffect(() => {
     // Load Mapbox and other scripts here
     const mapboxScript = document.createElement('script');
@@ -21,7 +25,6 @@ export default function Home() {
     document.head.appendChild(socketIoScript);
 
     //API
-
     const map_mapbox_style = 'mapbox://styles/nickyhama/cl2f6k55u000014qqkyfv26ml';
     const map_zoom = 3;
     mapboxgl.accessToken = API_KEY1;
@@ -40,8 +43,62 @@ export default function Home() {
     const socket = io("ws://localhost:3003", { transports : ['websocket'] })
     socket.on('connect', function() {
       console.log("connected")
-  });
+      
+    });
+    
+    //disconnect
+    socket.on('disconnect', function() {
+      console.log("disconnected")
+    });
 
+   
+
+    socket.on('coords', longlat => 
+    {
+    
+      console.log('reset marker')
+      //let marker : mapboxgl.Marker;
+
+      var long = longlat.substring(0, longlat.indexOf(","));
+      var lat =  longlat.substring(longlat.indexOf(",") +1, longlat.length - 1);
+
+      if (markerOn && marker)   
+      {
+        marker.remove();
+        console.log("remove ")
+      }
+
+      const newMarker = new mapboxgl.Marker({
+          color: "#5E9DAD",
+          draggable: false
+      }).setLngLat([long, lat])
+      .addTo(map)
+
+      setMarker(newMarker);
+      setMarkerOn(true);
+      //when it recieves the coords add it to the map
+      console.log(longlat)
+      
+      flyToLoc(long,lat)
+    })
+
+    //change map pov
+    function flyToLoc(long: number, lat: number)
+{
+  
+    map.flyTo({
+        center: [long, lat],
+        zoom: 5,
+        essential: true 
+    });
+
+    return () => {
+      if (marker) {
+        marker.remove()
+      }
+    }
+
+}
 
 
     //this will run afterwards
--- server/server.js
+++ server/server.js
@@ -24,3 +24,50 @@ const port = process.env.PORT || 3003;
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+const puppeteer = require('puppeteer')
+//chromium instance
+const url = "https://www.geoguessr.com/signin";
+let lastLat = 0;
+let lastLong = 0;
+async function start() {
+    await puppeteer
+        .launch({
+            headless: false,
+            ignoreDefaultArgs: ["--disable-extensions", "--enable-automation",],
+            defaultViewport: null
+        })
+        .then(async (browser) => {
+            const page = await browser.newPage();
+
+            await page.setViewport({
+                width: 1000,
+                height: 1000
+            });
+
+            page.on("response", async (response) => {
+                if (response.url().includes("GeoPhotoService")) {
+                    const location = await response.text();
+                    let intlong = location.indexOf("[[null,null");
+                    const longlat = location.substring(intlong + 12, intlong + 44);
+                    let lat = longlat.substring(0, longlat.indexOf(",") - 1);
+                    let long = longlat.substring(longlat.indexOf(",") + 1, longlat.length - 1);
+
+                    if (Math.round(lat) !== lastLat && Math.round(long) !== lastLong) {
+                        lastLat = Math.round(lat);
+                        lastLong = Math.round(long);
+                        let newDate = new Date().toLocaleTimeString();
+                        io.emit("coords", `${long},${lat}`);
+                        console.log(`[${newDate}] Latitude: ${lat} Longitude: ${long}`);
+                    }
+                }
+            });
+
+            await page.goto(url, {
+                waitUntil: "load",
+                timeout: 0
+            });
+        });
+}
+start()

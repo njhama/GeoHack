9adfe7672606545425d3bf8144554f83e8aec8a6
Nicky Hama <njhama@usc.edu>
2023-08-14 12:32:50 -0700

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// Replace 'YOUR_LOCAL_DB_URL' with your local MongoDB URL
 const mongoURI = 'mongodb://127.0.0.1:27017/GeoHack';
 
 const connectDB = async () => {
